3b13f01ffdb92d1fbf84633d6d7e2df788f9863a
rtwair <rtwair@gmail.com>
2019-05-01 15:36:11 +0800

    Collector completed with full camera and photo selection support - cordata also functions even for images

---
+++ collector/CreateCollectableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateCollectableViewController.swift
+//  collector
+//
+//  Created by Riyad on 1/5/19.
+//  Copyright © 2019 rtp. All rights reserved.
+//
+
+import UIKit
+
+class CreateCollectableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
